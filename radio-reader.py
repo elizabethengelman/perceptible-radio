c6882bc86fd9752cff84d2692965cb064817aeb3
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-10 10:52:19 -0400

    Use read_samples_async to continually ready samples from dongle

--- radio-reader.py
+++ radio-reader.py
@@ -1,41 +1,25 @@
 # This is based on the prototype for my Make Art with AI that used rtlsdr samples to manipulate the playback of some AI generated audio
 # https://pypi.org/project/pyrtlsdr/
 from rtlsdr import RtlSdr
+
+def callback(samples, context):
+    convertedSamples = []
+    for sample in samples:
+      convertedSamples.append([sample.real, sample.imag]) 
+
+    print("samples: ", convertedSamples) # put these on the queue instead
+
+
 sdr = RtlSdr()
 # configure device
 sdr.sample_rate = 2.048e6  # 2048000 Hz
 sdr.center_freq = 8.91e7  # 89100000 Hz
 sdr.freq_correction = 60   # PPM
 sdr.gain = 'auto'
-samples = sdr.read_samples(512)
-
-arrayOfSamples = []
-
-# each sample is a numpy.complex128, which has a real and an imaginary part, with methods for each
-# so to make this into something that I can use in javascript a bit more easier...
-for sample in samples:
-    arrayOfSamples.append([sample.real, sample.imag])
-
-json = {"data": arrayOfSamples}
-
-print(json)
-
+sdr.read_samples_async(callback, num_samples=512) # this defaults to reading 1024 samples
 
 """
-from this resource: https://arachnoid.com/software_defined_radios/
-"vectors are just numbers with two (or more) components" so i think that these I/Q data are vectors - it would be cool
-if i could use this and create a visualization like some of the vector tuts for p5js
-"The terms I and Q actually have a meaning — I means "Inphase" and Q means "Quadrature" (more detail). I mention this because the use of I for the real component, and Q
- for the imaginary component, of a complex number seems perversely confusing"
-"""
-
-"""
-# https://www.reddit.com/r/RTLSDR/comments/1xo5l3/help_me_make_sense_of_rtl2832u_raw_iq_data/
-* one answer here said that sdrs output quadrature waveform data - which means that each sample contains both magnitude
-  and phase info 
-* so in the case of RTL a sample contains an 8-bit (real) component and an 8-bit Q (imaginary) component, which represent
-  one point in time
-
-okay, this is definitely what my samples from Greenbank are, so that's exciting!!
-but like wtf do i do with that?
+Notes/thoughts/questions:
+* each sample is a numpy.complex128, which has a real and an imaginary part, with methods for each
+* I'm wondering if i should have the lights change for each sample set? or for each individual sample maybe ill just make that decision later
 """
