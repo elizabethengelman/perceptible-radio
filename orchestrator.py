f889f57a8d7647a1d0537ca530f775e02058cfdb
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-17 16:07:47 -0400

    Apply formatting changes

--- orchestrator.py
+++ orchestrator.py
@@ -41,24 +41,25 @@ def get_greenbank_data():
     # and the real-time data will be further split to send each individual sample to the queue one at a time
     samples_to_return = []
     for line in parsed_into_lines:
-      sample = line.split(", ")
-      # the first element looks like this: '[(0.12941176470588234-0.027450980392156876j)'
-      # and this is removing the left square bracket
-      sample[0] = sample[0][1:]
-      # the last element looks like this: '(-0.11372549019607847+0.08235294117647052j)]'
-      # and this is removing the right square bracket
-      sample[-1] = sample[-1][:-1]
-      samples_to_return.append(sample)
-      return samples_to_return
+        sample = line.split(", ")
+        # the first element looks like this: '[(0.12941176470588234-0.027450980392156876j)'
+        # and this is removing the left square bracket
+        sample[0] = sample[0][1:]
+        # the last element looks like this: '(-0.11372549019607847+0.08235294117647052j)]'
+        # and this is removing the right square bracket
+        sample[-1] = sample[-1][:-1]
+        samples_to_return.append(sample)
+        return samples_to_return
+
 
 def start_greenbank_feedbacker():
-    greenbank_data= get_greenbank_data()
+    greenbank_data = get_greenbank_data()
     for sample in greenbank_data:
         for individual_reading in sample:
-          complex_value = numpy.complex128(individual_reading)
-          sample = [complex_value.real, complex_value.imag]
-          f.change(sample)
-          time.sleep(2)
+            complex_value = numpy.complex128(individual_reading)
+            sample = [complex_value.real, complex_value.imag]
+            f.change(sample)
+            time.sleep(2)
 
 
 radio_reader_thread = threading.Thread(target=start_radio_reader)
