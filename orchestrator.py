daa86c4dffcd2b8af80219f8d89d52bc4f025bbd
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-17 13:05:54 -0400

    Read in and parse real greenbank data

--- orchestrator.py
+++ orchestrator.py
@@ -20,7 +20,7 @@ def start_radio_reader():
     reader.start()
 
 # def stop_radio_reader():
-  #can i do this by having the sdr instace be on the radioreader class, and then call cancel_ascyn whatever method 
+  # can i do this by having the sdr instace be on the radioreader class, and then call cancel_async whatever method
 
 
 def start_realtime_feedbacker():
@@ -30,9 +30,25 @@ def start_realtime_feedbacker():
         time.sleep(2)
 
 
+def get_greenbank_data():
+    april_23_file = open("./2023-04-23", "r")
+    april_23_data = april_23_file.read()
+    april_23_file.close()
+
+    parsed_data = april_23_data.split(", ")
+
+    # the first element looks like this: '[(0.12941176470588234-0.027450980392156876j)'
+    # and this is removing the left square bracket
+    parsed_data[0] = parsed_data[0][1:]
+
+    # the last element looks like this: '(-0.11372549019607847+0.08235294117647052j)]'
+    # and this is removing the right square bracket
+    parsed_data[-1] = parsed_data[-1][:-1]
+
+    return parsed_data
+
 def start_greenbank_feedbacker():
-    greenbank_data = [(0.26274509803921564+0.28627450980392166j), (-0.2313725490196078-0.16078431372549018j),
-                      (-0.0117647058823529+0.10588235294117654j), (-0.14509803921568631-0.19999999999999996j)]
+    greenbank_data= get_greenbank_data()
     for data_string in greenbank_data:
         complex_value = numpy.complex128(data_string)
         sample = [complex_value.real, complex_value.imag]
