bc215fd6adc33ed4947d9d4cec4f1907572e7045
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-17 18:23:44 -0400

    Calculate the signal power

--- feedbacker.py
+++ feedbacker.py
@@ -10,28 +10,35 @@ class Feedbacker:
         self.pixels = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS)
 
     def change(self, raw_data):
-        real_r_g_b_tuple = map_raw_data_to_r_g_b(raw_data.real)
-        imag_r_g_b_tuple = map_raw_data_to_r_g_b(raw_data.imag)
+        signal_power = get_signal_power(raw_data)
+        mapped_to_color_range = map_power_value_to_hex(signal_power)
+
+        rgb_tuple = hex_to_rgb(mapped_to_color_range)
+        inverse_rgb_tuple = get_inverse_color(rgb_tuple)
+
         for index, value in enumerate(self.pixels):
             if index % 2 != 0:
-                self.pixels[index] = real_r_g_b_tuple
+                self.pixels[index] = rgb_tuple
             else:
-                self.pixels[index] = imag_r_g_b_tuple
+                self.pixels[index] = inverse_rgb_tuple
 
 
-def map_raw_data_to_r_g_b(raw_data_value):
-    hex_value = map_raw_data_to_hex(raw_data_value)
-    return hex_to_rgb(hex_value)
+# TODO: move this into radio_reader?
+def get_signal_power(raw_data):
+    real_squared = raw_data.real ** 2
+    imag_squared = raw_data.imag ** 2
+    return real_squared + imag_squared
 
 
 def map_num_to_range(num, inMin, inMax, outMin, outMax):
     return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax - outMin))
 
 
-def map_raw_data_to_hex(raw_data_value):
+def map_power_value_to_hex(power_value):
     # it is possible for these raw values to be negative, so the range is -1 to 1
     # mapping the raw data to a range that fits in the hex color range
-    mapped_num = map_num_to_range(raw_data_value, -1, 1, 1000000, 16777215)
+    print("power: ", power_value)
+    mapped_num = map_num_to_range(power_value, -1, 1, 1000000, 16777215)
     rounded_mapped_number = round(mapped_num)
     # convert the mapped num decimal to a hex string, and then cut the x off the front
     return hex(rounded_mapped_number).split('x')[-1]
@@ -40,3 +47,13 @@ def map_raw_data_to_hex(raw_data_value):
 def hex_to_rgb(value):
     lv = len(value)
     return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
+
+
+def get_inverse_color(rgb_value):
+    original_r = rgb_value[0]
+    original_g = rgb_value[1]
+    original_b = rgb_value[2]
+    new_r = (original_r * -1) + 255
+    new_g = (original_g * -1) + 255
+    new_b = (original_b * -1) + 255
+    return (new_r, new_g, new_b)
