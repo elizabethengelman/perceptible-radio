981d07b13f739c4112847c31587c300cb43432de
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-18 13:52:11 -0400

    Add test audio clips to AudioGuider

--- audio_guider.py
+++ audio_guider.py
@@ -2,18 +2,25 @@ from playsound import playsound
 import threading
 import time
 
-
-def play_audio_1():
-    playsound("radio-waves-sound-walk-3.mp3")
-
-
-def play_audio_2():
-    time.sleep(0.5)
-    playsound("radio-waves-sound-walk-3.mp3")
+class AudioGuider:
+    def play_audio_1(self):
+        playsound("./audio/1-intro.m4a")
 
 
-play_audio_1_thread = threading.Thread(target=play_audio_1)
-play_audio_1_thread.start()
+    def play_audio_2(self):
+        playsound("./audio/2-real-time-data.m4a")
 
-play_audio_2_thread = threading.Thread(target=play_audio_2)
-play_audio_2_thread.start()
+
+    def play_audio_3(self):
+        playsound("./audio/3-greenbank-data.m4a")
+
+
+    def play_audio_4(self):
+        playsound("./audio/4-outro.m4a")
+
+
+# play_audio_1_thread = threading.Thread(target=play_audio_1)
+# play_audio_1_thread.start()
+
+# play_audio_2_thread = threading.Thread(target=play_audio_2)
+# play_audio_2_thread.start()
