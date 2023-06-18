9bb286c98cd36067fd58f7cad88099e4bd65e86e
Elizabeth Engelman <emengelman@gmail.com>
2023-06-18 16:39:50 -0400

    Try using pydub instead of playsound

--- audio_guider.py
+++ audio_guider.py
@@ -1,22 +1,27 @@
-from playsound import playsound
 import threading
 import time
+from pydub import AudioSegment
+from pydub.playback import play
 
 class AudioGuider:
     def play_audio_1(self):
-        playsound("./audio/1-intro.m4a")
+        audio_1 = AudioSegment.from_wav("./audio/1-intro.wav")
+        play(audio_1)
 
 
     def play_audio_2(self):
-        playsound("./audio/2-real-time-data.m4a")
+        audio_2 = AudioSegment.from_wav("./audio/2-real-time-data.wav")
+        play(audio_2)
 
 
     def play_audio_3(self):
-        playsound("./audio/3-greenbank-data.m4a")
+        audio_3 = AudioSegment.from_wav("./audio/3-greenbank-data.wav")
+        play(audio_3)
 
 
     def play_audio_4(self):
-        playsound("./audio/4-outro.m4a")
+        audio_4 = AudioSegment.from_wav("./audio/4-outro.wav")
+        play(audio_4)
 
 
 # play_audio_1_thread = threading.Thread(target=play_audio_1)
