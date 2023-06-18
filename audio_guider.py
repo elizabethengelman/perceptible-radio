9dd61122e6aba42691906e42f0eaf262bdaf6514
Elizabeth Engelman <emengelman@gmail.com>
2023-06-18 16:45:18 -0400

    Revert "Try using pydub instead of playsound"
    This reverts commit 9bb286c98cd36067fd58f7cad88099e4bd65e86e.

--- audio_guider.py
+++ audio_guider.py
@@ -1,27 +1,22 @@
+from playsound import playsound
 import threading
 import time
-from pydub import AudioSegment
-from pydub.playback import play
 
 class AudioGuider:
     def play_audio_1(self):
-        audio_1 = AudioSegment.from_wav("./audio/1-intro.wav")
-        play(audio_1)
+        playsound("./audio/1-intro.m4a")
 
 
     def play_audio_2(self):
-        audio_2 = AudioSegment.from_wav("./audio/2-real-time-data.wav")
-        play(audio_2)
+        playsound("./audio/2-real-time-data.m4a")
 
 
     def play_audio_3(self):
-        audio_3 = AudioSegment.from_wav("./audio/3-greenbank-data.wav")
-        play(audio_3)
+        playsound("./audio/3-greenbank-data.m4a")
 
 
     def play_audio_4(self):
-        audio_4 = AudioSegment.from_wav("./audio/4-outro.wav")
-        play(audio_4)
+        playsound("./audio/4-outro.m4a")
 
 
 # play_audio_1_thread = threading.Thread(target=play_audio_1)
