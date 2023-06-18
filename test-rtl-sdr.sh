e869c00dce1d00a86de033b3cd32d3f175e5b022
Elizabeth Engelman <emengelman@gmail.com>
2023-06-18 13:18:41 -0400

    Don't run the test-rtl-sdr script in the background

--- test-rtl-sdr.sh
+++ test-rtl-sdr.sh
@@ -4,4 +4,4 @@ modulation=fm
 samplerate=170k
 gain=50
 
-rtl_fm -f $frequency -M $modulation -s $samplerate -g $gain - | play -r $samplerate -t raw -e s -b 16 -c 1 - -q &
+rtl_fm -f $frequency -M $modulation -s $samplerate -g $gain - | play -r $samplerate -t raw -e s -b 16 -c 1 - -q
