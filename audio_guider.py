b2826193b08d1160b14cd83ce973b08b0ade7956
Elizabeth Engelman <elizabeth@redsquirrel.com>
2023-06-18 19:51:28 -0400

    Try pydub again so I can control the volume

--- audio_guider.py
+++ audio_guider.py
@@ -1,18 +1,24 @@
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 class AudioGuider:
     def play_background_music(self):
-        playsound("./audio/background.mp3")
+        background = AudioSegment.from_mp3("./audio/background.mp3")
+        play(background)
 
     def play_audio_1(self):
-        playsound("./audio/1-intro.m4a")
+        audio_1 = AudioSegment.from_wav("./audio/1-intro.wav")
+        play(audio_1)
 
     def play_audio_2(self):
-        playsound("./audio/2-real-time-data.m4a")
+        audio_2 = AudioSegment.from_wav("./audio/2-real-time-data.wav")
+        play(audio_2)
 
     def play_audio_3(self):
-        playsound("./audio/3-greenbank-data.m4a")
+        audio_3 = AudioSegment.from_wav("./audio/3-greenbank-data.wav")
+        play(audio_3)
 
     def play_audio_4(self):
-        playsound("./audio/4-outro.m4a")
+        audio_4 = AudioSegment.from_wav("./audio/4-outro.wav")
+        play(audio_4)
